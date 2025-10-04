565d8fdba49950215bfe6ddb82aaa30800e94b4d
Davi Ribeiro da Costa Lima <ribeiro-davi@hotmail.com>
2025-10-03 21:07:24 -0300

    Palindromo

---
+++ exer.py/exe3.py
@@ -0,0 +1,6 @@
+frase = 'Socorra-me, subir no ônibus em Marrocos'
+frase2 = frase[::-1]
+if frase2 == frase:
+    print('Palindromo')
+else:
+    print('Não é Palindromo')
---
+++ exer.py/exe4.py
@@ -0,0 +1,3 @@
+for contagem in range(10,21):
+    if contagem % 2 != 0:
+        print(contagem)
