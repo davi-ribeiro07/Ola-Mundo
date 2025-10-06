93602c184dd4b1161fe7b2caf850e1ddedf01de5
Davi Ribeiro da Costa Lima <ribeiro-davi@hotmail.com>
2025-10-06 12:12:04 -0300

    mudei o código

--- exer.py/exe4.py
+++ exer.py/exe4.py
@@ -1,4 +1,11 @@
-for contagem in range(10,21):
-    if contagem % 2 != 0:
-        print(contagem)
+for n in range(2,101):
+    if n in (2, 3, 5, 7):
+        print(n , 'é primo')
+    elif (n % 2 != 0 and n % 3 != 0 and n % 4 != 0 and n % 5 != 0 and
+        n % 6 != 0 and n % 7 != 0 and n % 8 != 0 and n % 9 != 0 and n % 10 != 0):
+        print(n, 'é primo')
+
+
+
+
         
