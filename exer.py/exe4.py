4f109bdb3560794ad6af51f1dab38833fa1d874a
Davi Ribeiro da Costa Lima <ribeiro-davi@hotmail.com>
2025-10-06 12:13:31 -0300

    Números Primos

--- exer.py/exe4.py
+++ exer.py/exe4.py
@@ -4,7 +4,6 @@ for n in range(2,101):
     elif (n % 2 != 0 and n % 3 != 0 and n % 4 != 0 and n % 5 != 0 and
         n % 6 != 0 and n % 7 != 0 and n % 8 != 0 and n % 9 != 0 and n % 10 != 0):
         print(n, 'é primo')
-        
 
 
 
